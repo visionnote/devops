577cb9ee02f9c69194052be622f4e3e64b53fa60
visionnote <42315977+visionnote@users.noreply.github.com>
2024-09-28 15:14:19 +0900

    처음 작업함

---
+++ src/main/java/kr/ac/koreatech/koreatech/KoreatechApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.koreatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/koreatech/koreatech/KoreatechApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.koreatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
